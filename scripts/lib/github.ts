bae510916bc880c604166bdc7fa83026dc02da19
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-04-09 12:01:03 +0700

    refactor: modular file and change env variabel

---
+++ scripts/lib/github.ts
@@ -0,0 +1,20 @@
+export async function fetchData(query: string) {
+  const token = process.env.GITHUB_TOKEN;
+  if (!token) throw new Error("GITHUB_TOKEN is missing. Check again,");
+
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  });
+
+  const result = await response.json();
+  if (result.errors) {
+    console.error(result.errors);
+    throw new Error("Graphql error!");
+  }
+  return result.data;
+}
