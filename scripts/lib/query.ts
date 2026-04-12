70ca3ad2c736fc1dc274b01bb0c70c8364105f1a
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-04-12 18:06:47 +0700

    feat: disable fork project

--- scripts/lib/query.ts
+++ scripts/lib/query.ts
@@ -2,7 +2,7 @@
 export const GITHUB_QUERY = `
   query {
     user(login: "aadnanmt") {
-      repositories(first: 100, ownerAffiliations: OWNER, orderBy: {field: PUSHED_AT, direction: DESC}) {
+      repositories(first: 100, ownerAffiliations: OWNER, isFork: false,  orderBy: {field: PUSHED_AT, direction: DESC}) {
         nodes {
           languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
             edges {
