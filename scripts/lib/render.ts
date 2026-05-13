ce10e0326d19f8b836c04adde8ee432c391abbeb
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-05-14 01:50:31 +0700

    refactor(scripts): implement template-driven architecture for README

--- scripts/lib/render.ts
+++ scripts/lib/render.ts
@@ -1,4 +1,6 @@
 // scripts/lib/render.ts
+
+// --- Bar ---
 export function makeBar(count: number, max: number, width: number) {
   const filledLength = Math.round((count / max) * width);
   const filled = "█".repeat(Math.max(0, filledLength));
@@ -6,54 +8,23 @@ export function makeBar(count: number, max: number, width: number) {
   return `[${filled}${empty}]`;
 }
 
+// --- Section Render   ---
 export function renderSection(title: string, line: string[]) {
   return [
     `$ aadnanmt-stats --${title.toLowerCase().replace(/\s+/g, "-")}`,
-    "-------------------------",
+    "------------------------------",
     ...line,
-    "-------------------------",
+    "------------------------------",
   ].join("\n");
 }
 
-export function buildReadme(statsOutput: string, commitOutput: string): string {
-  //
-  return `# Adnan (@aadnanmt)
-## 18yo dev n tech minimalist
-
-> **Current Mission**: Building high-performance edge infrastructure at \`nanoo-labs\`.
-
-### Tech environment now
-- **Frontend** — TS/JS (Astro), HTML, CSS (Tailwindcss)
-- **Backend** — TS (Hono) and Python (Fastapi, Flask).
-- **Sql** — Sqlite and Postgresql
-- **Nosql** — Redis
-- **OS** — Arch linux (Daily Driver)
-- **Window Manager** — Hyprland and I3wm
-- **Editor** — Neovim (Primary) and VS Code (Secondary)
-- **Shell and Terminal** — Fish Shell and Kitty
-- **Tools** — Git, Wrangler, Vite, Podman, Bun, Pnpm.
-
-### Dev activity
-\`\`\`text
-${statsOutput}
-
-${commitOutput}
-\`\`\`
-
----
-### Focus now
-- **Principles** • Performance-first. Zero-bloat. Scalable Architecture.
-- **Current Grind** • Internship (PKL) @ UIN Malang - Academic Dept.
-
----
-*Building fast, simple, and scalable apps with out-of-the-box thinking.*
-
-[ Portfolio ](https://aadnanmt.web.id) | [ Laboratory ](https://github.com/nanoo-labs)
-
----
-**Connect:**
-[X/Twitter](https://x.com/aadnanmt) / [Instagram](https://instagram.com/aadnanmt) / [Telegram](https://t.me/aadnanmtech) / [Email](mailto:dev@nanoo.cloud)<br>
-
-*[!] Automatically updated every 12 hours via custom Bun + TS script, thanks for reading*
-`;
+// --- Build README with inject  data to template ---
+export function buildReadme(
+  template: string,
+  statsOutput: string,
+  commitOutput: string,
+): string {
+  return template
+    .replace("{{languages}}", statsOutput)
+    .replace("{{commit}}", commitOutput);
 }
